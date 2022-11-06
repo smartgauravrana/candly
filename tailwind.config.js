0b51bafc75353214f72768ec7454afbcf6fa8e28
Gaurav Rana <smartgauravrana@gmail.com>
2022-11-06 18:17:22 +0530

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./common/**/*.{js,ts,jsx,tsx}",
   ],
   important: "#__next",
   theme: {
